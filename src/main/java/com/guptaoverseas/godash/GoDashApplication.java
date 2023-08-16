2116f454ba8704cb3cac9f47e15e3b2688f6cb81
Singh, Vassudha <vassudha_singh@optum.com>
2023-08-16 17:44:01 +0530

    initial commit structure

---
+++ src/main/java/com/guptaoverseas/godash/GoDashApplication.java
@@ -0,0 +1,13 @@
+package com.guptaoverseas.godash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoDashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoDashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guptaoverseas/godash/GoDashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guptaoverseas.godash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoDashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
